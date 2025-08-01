465673d7984d06956254465e19176c46a67408b8
AribNoceda <aribchaib7@gmail.com>
2025-08-01 06:34:26 +0100

    Added Collisions To Hills

--- Characters/player.gd
+++ Characters/player.gd
@@ -10,10 +10,10 @@ func _ready():
 	update_animation(start_direction)
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up"))
+		Input.get_action_strength("down") - Input.get_action_strength("up")).normalized()
 	
 	
 	update_animation(input_direction)
